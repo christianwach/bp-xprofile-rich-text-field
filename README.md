d1d6bf1a1442d05d11b2edf3905cba084a0ce2a2
Christian Wach <needle@haystack.co.uk>
2013-04-18 12:52:30 +0100

    refactored code

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
-bp-xprofile-rich-text-field
+BuddyPress Xprofile Rich Text Field
 ===========================
 
-Add a Rich-text Editor custom field type to Extended Profiles in BuddyPress.
+This WordPress plugin adds a Rich-text Editor custom field type to Extended Profiles in BuddyPress.
+
+## Installation ##
+
+### GitHub ###
+
+There are two ways to install from GitHub:
+
+#### ZIP Download ####
+
+If you have downloaded *BuddyPress Xprofile Rich Text Field* as a ZIP file from the GitHub repository, do the following to install and activate the plugin and theme:
+
+1. Unzip the .zip file and, if needed, rename the enclosing folder so that the plugin's files are located directly inside `/wp-content/plugins/bp-xprofile-rich-text-field`
+2. Activate the plugin
+4. You are done!
+
+#### git clone ####
+
+If you have cloned the code from GitHub, it is assumed that you know what you're doing.
+
+## Changelogs ##
+
+### 0.1 ###
+
+Initial commit
--- buddypress-xprofile-rich-text-field.php
+++ buddypress-xprofile-rich-text-field.php
@@ -1,14 +1,15 @@
-<?php
+<?php 
 /*
+--------------------------------------------------------------------------------
 Plugin Name: Buddypress Profile Rich Text Field
-Description: Add a Rich-text Editor custom field type to Extended Profiles in BuddyPress.
-Version: 1.0
+Description: Add a Rich Text custom field type (editable using the WordPress Visual Editor) to Extended Profiles in BuddyPress.
+Version: 0.1
 Author: Christian Wach
 Author URI: http://haystack.co.uk
 Plugin URI: http://haystack.co.uk
 --------------------------------------------------------------------------------
 Forked from: Buddypress Xprofile Custom Fields Type
-Original Author: Atallos Cloud
+Many thanks to the original author: Atallos Cloud
 Original Author URI: http://www.atallos.com/
 Original Plugin URI: http://www.atallos.com/portfolio/buddypress-xprofile-custom-fields-type/
 --------------------------------------------------------------------------------
@@ -16,325 +17,337 @@ Original Plugin URI: http://www.atallos.com/portfolio/buddypress-xprofile-custom
 
 
 
-/**
- * Register our field type
- */
-function bpxprofilertf_add_xprofile_field_type( $field_types ) {
+// set our version here - bumping this will cause CSS and JS files to be reloaded
+define( 'BP_XPROFILE_RICH_TEXT_FIELD_VERSION', '0.1' );
+
+
+
+/*
+--------------------------------------------------------------------------------
+BpXprofileRichTextField Class
+--------------------------------------------------------------------------------
+*/
+
+class BpXprofileRichTextField {
+
+	/** 
+	 * @description: initialises this object
+	 * @return object
+	 * @todo: 
+	 *
+	 */
+	function __construct() {
 	
-	// make sure we get an array
-	if ( is_array( $field_types ) ) {
-	
-		// append our item
-		$field_types[] = 'richtext';
+		// register field type
+		add_filter( 'xprofile_field_types', array( $this, 'register_field_type' ) );
 		
-	} else {
-
-		// set array with our item
-		$field_types = array( 'richtext' );
+		// preview field type
+		add_filter( 'xprofile_admin_field', array( $this, 'preview_admin_field'), 9, 2 );
 		
-	}
-	
-	// --<
-	return $field_types;
+		// test for a function in BP 1.7+
+		if ( function_exists( 'bp_is_network_activated' ) ) {
 
-}
-add_filter( 'xprofile_field_types', 'bpxprofilertf_add_xprofile_field_type' );
-
-
-
-
-
-/**
- * Preview our field type
- */
-function bpxprofilertf_xprofile_admin_field( $field, $echo = true ) {
-
-	// is it our type?
-	if ( $field->type == 'richtext' ) {
-		
-		// init
-		$html = '';
-		
-		// start buffering
-		ob_start();
-		
-		// get data and show
-		$data = BP_XProfile_ProfileData::get_value_byid( $field->id );
-
-		wp_editor( 
-		
-			esc_html( stripslashes( $data ) ), 
-			'xprofile_richtext',
-			array( 
-				'media_buttons' => false, 
-				'teeny' => true, 
-				'quicktags' => false,
-				'tinymce' => array(
-					'theme_advanced_buttons1' => 'bold,italic,underline,blockquote,strikethrough,link,unlink,spellchecker,removeformat,fullscreen',
-					'theme_advanced_buttons2' => '',
-					'theme_advanced_buttons3' => ''
-				)
-			)
+			// in BP 1.7+ show our field type in edit mode via pre_visibility hook
+			add_action( 'bp_custom_profile_edit_fields_pre_visibility', array( $this, 'edit_field' ) );
 			
-		);
-		
-		// clean up
-		$html = ob_get_contents();
-		ob_end_clean();
-		
-		if ( $echo ) {
-			echo $html;
-			return;
 		} else {
-			return $html;
+		
+			// show our field type in edit mode via the previous hook
+			add_action( 'bp_custom_profile_edit_fields', array( $this, 'edit_field' ) );
+			
 		}
+		
+		// show our field type in read mode after all BuddyPress filters
+		add_filter( 'bp_get_the_profile_field_value', array( $this, 'get_field_value' ), 30, 3 );
+		
+		// filter for those who use xprofile_get_field_data instead of get_field_value
+		add_filter( 'xprofile_get_field_data', array( $this, 'get_field_data' ), 15, 3 );
+		
+		// enqueue javascript on admin screens
+		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_js') );
+
+		// enqueue stylesheet on public-facing pages
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_css') );
+
+		// --<
+		return $this;
 
 	}
 
-}
-add_filter( 'xprofile_admin_field', 'bpxprofilertf_xprofile_admin_field', 9 );
-
-
-
-
-
-/**
- * Show our field type in edit mode
- */
-function bpxprofilertf_edit_xprofile_field( $echo = true ) {
-	
-	// only for our filed type...
-	if ( bp_get_the_profile_field_type() == 'richtext' ) {
-
-		global $field;
+	/**
+	 * PHP 4 constructor
+	 */
+	function BpXprofileRichTextField() {
 		
-		// start buffering
-		ob_start();
+		// is this php5?
+		if ( version_compare( PHP_VERSION, "5.0.0", "<" ) ) {
 		
-		// init data
-		$data = '';
-		
-		// do we have data yet?
-		if ( isset( $field->data->value ) ) { 
-			
-			// yes, grab it
-			$data = $field->data->value; 
+			// call php5 constructor
+			$this->__construct();
 			
 		}
+		
+		// --<
+		return $this;
 
-		?>
-		<div class="input-richtext">
-			<label class="label-form <?php if ( bp_get_the_profile_field_is_required() ) : ?>required<?php endif; ?>" for="<?php bp_the_profile_field_input_name() ?>"><?php bp_the_profile_field_name() ?> <?php if ( bp_get_the_profile_field_is_required() ) { echo __('*', 'bpxprofilertf'); } ?></label>
-			<?php
+	}
+
+	//##########################################################################
+	
+	/**
+	 * Register our field type
+	 */
+	function register_field_type( $field_types ) {
+	
+		// make sure we get an array
+		if ( is_array( $field_types ) ) {
+	
+			// append our item
+			$field_types[] = 'richtext';
+		
+		} else {
+
+			// set array with our item
+			$field_types = array( 'richtext' );
+		
+		}
+	
+		// --<
+		return $field_types;
+
+	}
+
+	/**
+	 * Preview our field type
+	 */
+	function preview_admin_field( $field, $echo = true ) {
+
+		// is it our type?
+		if ( $field->type == 'richtext' ) {
+		
+			// init
+			$html = '';
+		
+			// start buffering
+			ob_start();
+		
+			// get data and show
+			$data = BP_XProfile_ProfileData::get_value_byid( $field->id );
 			
 			// define our editor
 			wp_editor( 
-			
-				wpautop( stripslashes( $data ) ),
-				bp_get_the_profile_field_input_name(),
-				array(
+		
+				esc_html( stripslashes( $data ) ), 
+				'xprofile_richtext',
+				array( 
 					'media_buttons' => false, 
 					'teeny' => true, 
 					'quicktags' => false,
 					'tinymce' => array(
-						'theme_advanced_buttons1' => 'bold,italic,underline,blockquote,strikethrough,|,link,unlink,|,spellchecker,removeformat,fullscreen',
+						'theme_advanced_buttons1' => 'bold,italic,underline,blockquote,strikethrough,link,unlink,spellchecker,removeformat,fullscreen',
 						'theme_advanced_buttons2' => '',
 						'theme_advanced_buttons3' => ''
 					)
 				)
-				
+			
 			);
+		
+			// clean up
+			$html = ob_get_contents();
+			ob_end_clean();
+		
+			if ( $echo ) {
+				echo $html;
+				return;
+			} else {
+				return $html;
+			}
+
+		}
+
+	}
+
+	/**
+	 * Show our field type in edit mode
+	 */
+	function edit_field() {
+	
+		// only for our filed type...
+		if ( bp_get_the_profile_field_type() == 'richtext' ) {
+
+			global $field;
+		
+			// init data
+			$data = '';
+		
+			// do we have data yet?
+			if ( isset( $field->data->value ) ) { 
+			
+				// yes, grab it
+				$data = $field->data->value; 
+			
+			}
+
+			// start buffering
+			ob_start();
+		
+			?>
+			<div class="input-richtext">
+				<label class="label-form <?php if ( bp_get_the_profile_field_is_required() ) { ?>required<?php } ?>" for="<?php bp_the_profile_field_input_name() ?>"><?php bp_the_profile_field_name() ?> <?php if ( bp_get_the_profile_field_is_required() ) { echo __('*', 'bpxprofilertf'); } ?></label>
+				<?php
+			
+				// define our editor
+				wp_editor( 
+			
+					wpautop( stripslashes( $data ) ),
+					bp_get_the_profile_field_input_name(),
+					array(
+						'media_buttons' => false, 
+						'teeny' => true, 
+						'quicktags' => false,
+						'tinymce' => array(
+							'theme_advanced_buttons1' => 'bold,italic,underline,blockquote,strikethrough,|,link,unlink,|,spellchecker,removeformat,fullscreen',
+							'theme_advanced_buttons2' => '',
+							'theme_advanced_buttons3' => ''
+						)
+					)
+				
+				);
 			
 			?>
-		</div>
-		<?php
-		
-		// clean up
-		$output = ob_get_contents();
-		ob_end_clean();
-
-		if ( $echo ) {
+			</div>
+			<?php
+			
+			// clean up
+			$output = ob_get_contents();
+			ob_end_clean();
+			
+			// print to screen
 			echo $output;
+
+		} 
+
+	}
+
+	/**
+	 * Show our field type in read mode
+	 */
+	function get_field_value( $value = '', $type = '', $id = '' ) {
+	
+		// is it our field type?
+		if ( $type == 'richtext' ) {
+	
+			// we want the raw data, unfiltered
+			global $field;
+			$value = $field->data->value;
+		
+			// now, apply basic content filters
+			$value = wpautop( convert_chars( wptexturize( stripslashes( $value ) ) ) );
+		
+		}
+	
+		// --<
+		return $value;
+
+	}
+
+	/**
+	 * Filter for those who use xprofile_get_field_data instead of get_field_value.
+	 * @param type $value
+	 * @param type $field_id
+	 * @return string
+	 */
+	function get_field_data( $value = '', $field_id = '', $user_id = '' ) {
+	
+		// get field object
+		$field = new BP_XProfile_Field( $field_id );
+	
+		// is it ours?
+		if ( $field->type == 'richtext' ) {
+
+			// apply basic content filters
+			$value = wpautop( convert_chars( wptexturize( stripslashes( $value ) ) ) );
+
+		}
+	
+		// --<
+		return $value;
+
+	}
+
+	/**
+	 * JS files
+	 */
+	function enqueue_js( $hook ) {    
+		
+		// only enqueue scripts on appropriate BP pages
+		if ( 'users_page_bp-profile-setup' != $hook AND 'buddypress_page_bp-profile-setup' != $hook ) {
 			return;
-		} else {
-			return $output;
 		}
-
-	} 
-
-}
-add_action( 'bp_custom_profile_edit_fields_pre_visibility', 'bpxprofilertf_edit_xprofile_field' );
-
-
-
-
-
-
-/**
- * Show our field type in read mode
- */
-function bpxprofilertf_get_field_value( $value='', $type='', $id='') {
-	
-	// is it our field type?
-	if ( $type == 'richtext' ) {
-	
-		// we want the raw data, unfiltered
-		global $field;
-		$value = $field->data->value;
 		
-		// apply basic content filters
-		$value = wpautop( convert_chars( wptexturize( stripslashes( $value ) ) ) );
+		// enqueue it
+		wp_enqueue_script( 
+
+			'bpxprofilertf-js', 
+			plugins_url( 'assets/js/buddypress-xprofile-rich-text-field.js', __FILE__ ), 
+			array( 'jquery' ), // deps
+			BP_XPROFILE_RICH_TEXT_FIELD_VERSION // version
+
+		);
 		
-	}
-	
-	// --<
-	return $value;
+		// define translatable strings
+		$params = array(
+			'richtext' => __( 'Rich Text', 'bpxprofilertf' )
+		);
 
-}
-add_filter( 'bp_get_the_profile_field_value', 'bpxprofilertf_get_field_value', 30, 3 );
-
-
-
-
-
-
-/**
- * Filter for those who use xprofile_get_field_data instead of get_field_value.
- * @param type $value
- * @param type $field_id
- * @return string
- */
-function bpxprofilertf_get_field_data( $value, $field_id, $user_id ) {
-	
-	// get field object
-    $field = new BP_XProfile_Field( $field_id );
-	
-	// is it ours?
-	if ( $field->type == 'richtext' ) {
-
-		// apply basic content filters
-		$value = wpautop(convert_chars(wptexturize( stripslashes( $value ) )));
+		// localise
+		wp_localize_script(
+		
+			'bpxprofilertf-js',
+			'RichTextParams',
+			$params
+			
+		);
 
 	}
-	
-	// --<
-	return $value;
 
-}
-add_filter( 'xprofile_get_field_data', 'bpxprofilertf_get_field_data', 15, 3 );
+	/**
+	 * CSS files
+	 */
+	function enqueue_css() {    
+		
+		// enqueue
+		wp_enqueue_style( 
 
+			'bpxprofilertf-css', 
+			plugins_url( 'assets/css/buddypress-xprofile-rich-text-field.css', __FILE__ ), 
+			array( 'bp-legacy-css' ), // deps
+			BP_XPROFILE_RICH_TEXT_FIELD_VERSION, // version
+			'all' // media
 
+		);
 
-
-
-/**
- * Function replacing the original buddypress filter.
- * @param type $field_value
- * @param type $field_type
- * @return string
- */
-function bpxprofilertf_xprofile_filter_link_profile_data( $field_value, $field_type = 'textbox' ) {
-	if ( 'richtext' == $field_type)
-		return $field_value;
-
-	if ( !strpos( $field_value, ',' ) && ( count( explode( ' ', $field_value ) ) > 5 ) )
-		return $field_value;
-
-	$values = explode( ',', $field_value );
-
-	if ( !empty( $values ) ) {
-		foreach ( (array) $values as $value ) {
-			$value = trim( $value );
-
-			// If the value is a URL, skip it and just make it clickable.
-			if ( preg_match( '@(https?://([-\w\.]+)+(:\d+)?(/([\w/_\.]*(\?\S+)?)?)?)@', $value ) ) {
-				$new_values[] = make_clickable( $value );
-
-			// Is not clickable
-			} else {
-
-				// More than 5 spaces
-				if ( count( explode( ' ', $value ) ) > 5 ) {
-					$new_values[] = $value;
-
-				// Less than 5 spaces
-				} else {
-					$search_url   = add_query_arg( array( 's' => urlencode( $value ) ), bp_get_members_directory_permalink() );
-					$new_values[] = '<a href="' . $search_url . '" rel="nofollow">' . $value . '</a>';
-				}
-			}
-		}
-
-		$values = implode( ', ', $new_values );
 	}
 
-	return $values;
-}
-
-/**
- * Replacing the buddypress filter link profile is it has the filter.
- * If user deactivated the filter, we don't add another filter.
- */
-function bpxprofilertf_remove_xprofile_links() {
-    if (has_filter('bp_get_the_profile_field_value', 'xprofile_filter_link_profile_data')) {
-        remove_filter( 'bp_get_the_profile_field_value', 'xprofile_filter_link_profile_data', 9, 2 );
-        add_filter( 'bp_get_the_profile_field_value', 'bpxprofilertf_xprofile_filter_link_profile_data', 9, 2);
-    }
-}
-add_action( 'bp_setup_globals', 'bpxprofilertf_remove_xprofile_links', 9999 );
+} // class ends
 
 
 
 
 
 /**
- * JS files
+ * Initialise our plugin after BuddyPress
  */
-function bpxprofilertf_add_js($hook) {    
+function bp_xprofile_rich_text_field() {    
 
-	if ('users_page_bp-profile-setup' != $hook && 'buddypress_page_bp-profile-setup' != $hook)
-		return;
-
-	wp_enqueue_script( 
-
-		'bpxprofilertf-js', 
-		plugins_url( 'assets/js/buddypress-xprofile-rich-text-field.js', __FILE__ ), 
-		array( 'jquery' ), // deps
-		'1.0' // version
-
-	);
-
-	$params = array(
-
-		'richtext' => __('Rich Text', 'bpxprofilertf')
-
-	);
-
-	wp_localize_script('bpxprofilertf-js', 'RichTextParams', $params);
+	// init plugin
+	$bp_xprofile_rich_text_field = new BpXprofileRichTextField;
 
 }
-add_action( 'admin_enqueue_scripts', 'bpxprofilertf_add_js' );
 
+// add action for plugin init
+add_action( 'bp_init', 'bp_xprofile_rich_text_field' );
 
-
-
-/**
- * CSS files
- */
-function bpxprofilertf_add_css($hook) {    
-
-	wp_enqueue_style( 
-
-		'bpxprofilertf-css', 
-		plugins_url( 'assets/css/buddypress-xprofile-rich-text-field.css', __FILE__ ), 
-		array( 'bp-legacy-css' ), // deps
-		'1.0', // version
-		'all' // media
-
-	);
-
-}
-add_action( 'wp_enqueue_scripts', 'bpxprofilertf_add_css' );
 
 
 
